3d179ed58b490c6c64b1b2b0b9041fbb48b8e488
yugi <msaiyugandhar333@gmail.com>
2025-09-01 21:54:24 +0530

    made changes to contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export default class Footer extends Component {
               <a href="/">hello how are you</a>
             </li>
             <li>
-              <a href="/">Contact</a>
+              <a href="/">Contact Us</a>
             </li>
             <li>
               <a href="/">Terms of Use</a>
